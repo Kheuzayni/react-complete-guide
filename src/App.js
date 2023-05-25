43a37ad8fc8cb3c6a03e1341040474878e0d77ff
BAYE CHEIKH FALL <c.baye@attijariwafa.com>
2023-05-25 09:58:46 +0100

    2eme methode branchement comportementet affichage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [compteur, setCompteur] = useState(1);
   //comportements
   const handleClick = () => {
-    alert("hangleclick done");
+    alert("hangleclick done : 1ere methode branchement composant et affichage");
   };
 
   //affichage (render)
@@ -15,7 +15,8 @@ function App() {
   return (
   <div>
     <h1>{compteur}</h1>
-    <button onClick={handleClick}>Alert</button>
+    <button onClick={handleClick}>methode1</button> 
+    <button onClick={() => alert("hangleclick done : 2eme methode branchement composant et affichage")}>methode2</button>
   </div>
   );
   
