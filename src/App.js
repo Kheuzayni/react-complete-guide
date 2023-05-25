c4368bb4ff07fd7a4e88c996c3d62ec33377fcbd
BAYE CHEIKH FALL <c.baye@attijariwafa.com>
2023-05-25 10:13:59 +0100

    Add somes exo

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ function App() {
   //state (état, données)
   const [compteur, setCompteur] = useState(1);
   //comportements
-  const handleClick = () => {
+  const handleClick1 = () => {
     alert("hangleclick done : 1ere methode branchement composant et affichage");
+  };
+  const handleClick = () => {
+    //compteur = compteur+1
+    setCompteur(compteur+1);
   };
 
   //affichage (render)
-  
   return (
   <div>
+    <ExerciseComponent/>
     <h1>{compteur}</h1>
-    <button onClick={handleClick}>methode1</button> 
-    <button onClick={() => alert("hangleclick done : 2eme methode branchement composant et affichage")}>methode2</button>
+    <button onClick={handleClick1}>methode1</button> 
+    <button onClick={() => alert("hangleclick done : 2eme methode branchement composant et affichage")}>
+      methode2
+    </button><br/>
+    <button onClick={handleClick}>Incrémente</button>
   </div>
   );
   
