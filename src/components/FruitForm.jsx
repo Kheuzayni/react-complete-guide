4412b9583a185c9d747a1be32776c563b7fc8bbe
BAYE CHEIKH FALL <c.baye@attijariwafa.com>
2023-05-26 10:23:58 +0100

    composant fruit

--- src/components/FruitForm.jsx
+++ src/components/FruitForm.jsx
@@ -1,9 +1,32 @@
-export default function FruitForm(){
+import { useState } from "react";
+
+export default function FruitForm(fruits, setFruits){
     //state
-
+    const [newFruit, setNewFruit] = useState("");
     //Comportements
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //Process
+        //1. copie du state
+        const fruitsCopy = [...fruits];
+        //2. manipuler ma copie
+        const id = new Date().getTime();
+        const nom = newFruit;
+        fruitsCopy.push( {id, nom} );
+        //3. Update mon state avec le setter
+        setFruits(fruitsCopy) ;
+        //4. On vide le input 
+        setNewFruit("");
+    
+      }
+      const handleChange = (event) => {
+        setNewFruit(event.targert.value);
+      }
     //Affichages (render)
-    return <div>Fruits</div>
-
+    return (
+        <form action="submit" onSubmit={handleSubmit}>
+            <input type="text" placeholder="ajouter un fruit..." onChange={handleChange} value={newFruit} />
+            <button >+ Add fruit</button>
+        </form>
+    );
 }
